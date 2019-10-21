45bf4f89d6667cf606afa4fd32fc619d5551d4e8
espFabioAlmeida <teg.fabio.almeida@gmail.com>
2019-10-21 10:43:18 -0300

    implementado formularios de boleto e credito

--- src/app/form-pagamento/form-pagamento.component.ts
+++ src/app/form-pagamento/form-pagamento.component.ts
@@ -11,19 +11,45 @@ export class FormPagamentoComponent implements OnInit {
   formularioValor: FormGroup;
   formularioCredito: FormGroup;
   formularioBoleto: FormGroup;
+  valorTaxas: any;
+  valorTaxasTexto: String;
+
   constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.iniciaFormulario();
+    this.valorTaxas = 0;
   }
 
+  calculaValor()
+  {
+    //Calcula o valor da compra (multiplica por 100 para aplicar mascara
+    this.valorTaxas = this.formularioValor.value.taxas * 27.50 * 100; 
+
+    //Aplicação de máscara
+    if(!(this.valorTaxas % 10) && !(this.valorTaxas % 100)) //Valor cheio
+    {
+      this.valorTaxasTexto = "" + this.valorTaxas / 100 + ".00";
+      this.valorTaxas /= 100;
+      return;
+    }
+
+    if(!(this.valorTaxas % 10)) //Valor multiplo de .1 (27.5, 12.8)
+    {
+      this.valorTaxasTexto = "" + this.valorTaxas / 100 + "0";
+      this.valorTaxas /= 100;
+      return;
+    }
+    
+    //Valor já possui 2 casas decimais
+    this.valorTaxasTexto = "" + this.valorTaxas / 100;
+    this.valorTaxas /= 100;
+  }
 
   iniciaFormulario()
   {
     this.formularioValor = this.formBuilder.group({
       taxas: [null, [Validators.required]],
-
-      valor: [null, [Validators.required]],
 
       formaPagamento: ["SELECIONE", [Validators.required]],
     });
@@ -41,9 +67,9 @@ export class FormPagamentoComponent implements OnInit {
         Validators.minLength(5),
         Validators.maxLength(40)],],
 
-      mesVencimento: [null, [Validators.required],],
+      mesVencimento: ["01", [Validators.required,],],
 
-      anoVencimento: [null, [Validators.required],],
+      anoVencimento: ["2023", [Validators.required],],
     })
 
     this.formularioBoleto = this.formBuilder.group({
